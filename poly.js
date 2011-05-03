c5521f15de95ed3109f6900e3eb231313d385319
unscriptable <john@e-numera.com>
2011-05-02 23:26:04 -0400

    first draft

---
+++ poly.js
@@ -0,0 +1,48 @@
+/**
+ * polyfill / shim plugin for AMD loaders
+ *
+ * (c) copyright 2011, unscriptable.com / John Hann
+ * Licensed under the MIT License at:
+ * 		http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+
+define(['exports'], function (exports) {
+
+	exports.load = function (def, require, onload, config) {
+
+		function success (module) {
+			// check for curl.js's promise
+			onload.resolve ? onload.resolve(module) : onload(module);
+		}
+
+		function fail (ex) {
+			// check for curl.js's promise
+			if (onload.reject) {
+				onload.reject(ex)
+			}
+			else {
+				throw ex;
+			}
+		}
+
+
+		// load module
+		require([def], function (module) {
+
+			try {
+				// augment native
+				if (module.polyfill) {
+					module.ployfill();
+				}
+			}
+			catch (ex) {
+				fail(ex);
+			}
+			success(module);
+
+		});
+
+	}
+
+});
