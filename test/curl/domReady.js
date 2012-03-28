35a063ac1401cc98eb2937576905a41fa236534b
John Hann <john@e-numera.com>
2012-03-27 21:37:20 -0400

    updated to latest curl.js and domReady!

--- test/curl/domReady.js
+++ test/curl/domReady.js
@@ -1,17 +1,25 @@
+/** MIT License (c) copyright B Cavalier & J Hann */
+
 /**
  * curl domReady
  *
- * (c) copyright 2011, unscriptable.com / John Hann
  * Licensed under the MIT License at:
  * 		http://www.opensource.org/licenses/mit-license.php
- *
+ */
+
+/**
  * usage:
- *  require(['ModuleA'], function (ModuleA, domReady) {
+ *  require(['ModuleA', 'curl/domReady'], function (ModuleA, domReady) {
  * 		var a = new ModuleA();
- * 		document.body.appendChild(a.domNode);
+ * 		domReady(function () {
+ * 			document.body.appendChild(a.domNode);
+ * 		});
  * 	});
  *
- * HT to Bryan Forbes: http://www.reigndropsfall.net/
+ * also: check out curl's domReady! plugin
+ *
+ * HT to Bryan Forbes who wrote the initial domReady code:
+ * http://www.reigndropsfall.net/
  *
  */
 (function (global, doc) {
@@ -19,18 +27,22 @@
 	var
 		readyState = 'readyState',
 		// keep these quoted so closure compiler doesn't squash them
-		readyStates = { 'loaded': 1, /*'interactive': 1,*/ 'complete': 1 },
+		readyStates = { 'loaded': 1, 'interactive': 1, 'complete': 1 },
 		callbacks = [],
-		fixReadyState = typeof doc[readyState] != "string",
-		i = 0,
+		fixReadyState = doc && typeof doc[readyState] != "string",
 		// IE needs this cuz it won't stop setTimeout if it's already queued up
 		completed = false,
-		addEvent, remover, removers = [], pollerTO;
+		pollerTime = 10,
+		addEvent,
+		remover,
+		removers = [],
+		pollerHandle,
+		undef;
 
 	function ready () {
 		completed = true;
-		clearTimeout(pollerTO);
-		while (remover = removers[i++]) remover();
+		clearTimeout(pollerHandle);
+		while (remover = removers.pop()) remover();
 		if (fixReadyState) {
 			doc[readyState] = "complete";
 		}
@@ -41,43 +53,70 @@
 		}
 	}
 
+	var testEl;
+	function isDomManipulable () {
+		// question: implement Diego Perini's IEContentLoaded instead?
+		// answer: The current impl seems more future-proof rather than a
+		// non-standard method (doScroll). i don't care if the rest of the js
+		// world is using doScroll! They can have fun repairing their libs when
+		// the IE team removes doScroll in IE 13. :)
+		if (!doc.body) return false; // no body? we're definitely not ready!
+		if (!testEl) testEl = doc.createTextNode('');
+		try {
+			// webkit needs to use body. doc
+			doc.body.removeChild(doc.body.appendChild(testEl));
+			testEl = undef;
+			return true;
+		}
+		catch (ex) {
+			return false;
+		}
+	}
+
 	function checkDOMReady (e) {
-		if (!completed && readyStates[doc[readyState]]) {
+		var isReady;
+		// all browsers except IE will be ready when readyState == 'interactive'
+		// so we also must check for document.body
+		isReady = readyStates[doc[readyState]] && isDomManipulable();
+		if (!completed && isReady) {
 			ready();
 		}
+		return isReady;
 	}
 
 	function poller () {
 		checkDOMReady();
 		if (!completed) {
-			pollerTO = setTimeout(poller, 30);
+			pollerHandle = setTimeout(poller, pollerTime);
 		}
 	}
 
 	// select the correct event listener function. all of our supported
-	// browser will use one of these
-	addEvent = ('addEventListener' in global) ?
-		function (node, event) {
+	// browsers will use one of these
+	if ('addEventListener' in global) {
+		addEvent = function (node, event) {
 			node.addEventListener(event, checkDOMReady, false);
 			return function () { node.removeEventListener(event, checkDOMReady, false); };
-		} :
-		function (node, event) {
+		};
+	}
+	else {
+		addEvent = function (node, event) {
 			node.attachEvent('on' + event, checkDOMReady);
 			return function () { node.detachEvent(event, checkDOMReady); };
 		};
-
-	if (doc[readyState] == "complete") {
-		ready();
 	}
-	else {
-		// add event listeners and collect remover functions
-		removers = [
-			addEvent(global, 'load'),
-			addEvent(doc, 'readystatechange'),
-			addEvent(global, 'DOMContentLoaded')
-		];
-		// additionally, poll for readystate
-		pollerTO = setTimeout(poller, 30);
+
+	if (doc) {
+		if (!checkDOMReady()) {
+			// add event listeners and collect remover functions
+			removers = [
+				addEvent(global, 'load'),
+				addEvent(doc, 'readystatechange'),
+				addEvent(global, 'DOMContentLoaded')
+			];
+			// additionally, poll for readystate
+			pollerHandle = setTimeout(poller, pollerTime);
+		}
 	}
 
 	define(/*=='curl/domReady',==*/ function () {
@@ -93,4 +132,4 @@
 
 	});
 
-}(this, document));
+}(this, this.document));
