27987f499d170fb112360249845b24564054653e
John Hann <john@e-numera.com>
2012-05-11 22:47:06 -0400

    improved `getPrototypeOf`, `getOwnPropertyDescriptor`, and `create` compatibility; created real shim for `isExtensible`

--- object.js
+++ object.js
@@ -9,19 +9,46 @@
  * 		http://www.opensource.org/licenses/mit-license.php
  */
 /**
- * The goal of these shims is to emulate a JavaScript 1.8.5 environment as
- * much as possible.  While it's not feasible to fully shim the Object object,
+ * The goal of these shims is to emulate a JavaScript 1.8.5+ environments as
+ * much as possible.  While it's not feasible to fully shim Object,
  * we can try to maximize code compatibility with older js engines.
  *
  * Note: these shims cannot fix `for (var p in obj) {}`. Instead, use this:
  *     Object.keys(obj).forEach(function (p) {}); // shimmed Array
- * or
- *     array.forEach(Object.keys(obj), function (p) {}); // wrapped Array
  *
  * Also, these shims can't prevent writing to object properties.
  *
- * TODO: isExtensible and preventExtensions
- * TODO: devise workable shims? for freeze, seal, etc.
+ * If you want your code to fail loudly if a shim can't mimic ES5 closely
+ * then set the AMD loader config option `failIfShimmed`.  Possible values
+ * for `failIfShimmed` include:
+ *
+ * true: fail on every shimmed Object function
+ * false: fail never
+ * RegExp: fail for shims whose name matches the RegExp
+ * string: string is converted to a RegExp
+ * function: fail for shims whose name returns true from function (name) {}
+ *
+ * By default, failIfShimmed fails for the following functions:
+ * defineProperty
+ * defineProperties
+ * preventExtensions
+ * getOwnPropertyDescriptor
+ *
+ * The following functions don't fail by default because they're safely shimmed:
+ * create (unless the second parameter is specified since that calls defineProperties)
+ * keys
+ * getOwnPropertyNames
+ * getPrototypeOf
+ * isExtensible
+ *
+ * In order to play nicely with several third-party libs (including Promises/A
+ * implementations), the following functions don't fail by default even though
+ * they can't be correctly shimmed:
+ * freeze
+ * seal
+ * isFrozen
+ * isSealed
+ *
  * TODO: resolve IE8's partial defineProperty impl? (works for DOM nodes)
  *
  */
@@ -32,13 +59,15 @@ define(['./_base', 'exports'], function (base, exports) {
 		methods = {},
 		missing = {},
 		refObj = Object,
+		refProto = refObj.prototype,
 		getPrototypeOf,
 		featureMap,
-		failTestRx;
+		failTestRx,
+		undef;
 
 	getPrototypeOf = typeof {}.__proto__ == 'object'
 		? function (object) { return object.__proto__; }
-		: function (object) { return object.constructor.prototype; };
+		: function (object) { return object.constructor ? object.constructor.prototype : refProto; };
 
 	featureMap = {
 		'object-create': { prop: 'create', impl: create },
@@ -56,7 +85,7 @@ define(['./_base', 'exports'], function (base, exports) {
 		'object-getownpropertydescriptor': { prop: 'getOwnPropertyDescriptor', impl: getOwnPropertyDescriptor }
 	};
 
-	failTestRx = /^define|^isextensible|^prevent|descriptor$/;
+	failTestRx = /^define|^prevent|descriptor$/;
 
 	function RegexpShouldThrow (feature) {
 		return failTestRx.test(feature);
@@ -75,11 +104,21 @@ define(['./_base', 'exports'], function (base, exports) {
 
 	function PolyBase () {}
 
-	function create (prototype, props) {
+	// for better compression
+	function hasProp (object, name) {
+		return object.hasOwnProperty(name);
+	}
+
+	function create (proto, props) {
 		var obj;
-		PolyBase.prototype = prototype;
-		obj = new PolyBase(props);
-		PolyBase.prototype = null;
+		if (proto) {
+			PolyBase.prototype = proto;
+			obj = new PolyBase(props);
+			PolyBase.prototype = null;
+		}
+		else {
+			obj = {};
+		}
 		if (arguments.length > 1) {
 			// defineProperties could throw depending on `shouldThrow`
 			defineProperties(obj, props);
@@ -88,6 +127,7 @@ define(['./_base', 'exports'], function (base, exports) {
 	}
 
 	function freeze (object) {
+		// TODO: determine if we need to fix Rhino bug: Object.freeze(function () {});
 		return object;
 	}
 
@@ -103,22 +143,18 @@ define(['./_base', 'exports'], function (base, exports) {
 		return false;
 	}
 
-	function _getPropsArray (object, own) {
+	function keys (object) {
 		var result = [];
 		for (var p in object) {
-			if (!own || object.hasOwnProperty(p)) {
+			if (hasProp(object, p)) {
 				result.push(p);
 			}
 		}
 		return result;
 	}
 
-	function keys (object) {
-		return _getPropsArray(object, true);
-	}
-
 	function getOwnPropertyNames (object) {
-		return _getPropsArray(object, true);
+		return keys(object);
 	}
 
 	function defineProperty(object, name, descriptor) {
@@ -127,16 +163,26 @@ define(['./_base', 'exports'], function (base, exports) {
 	}
 
 	function defineProperties(object, descriptors) {
-		var i = 0,
-			names = keys(descriptors);
-		for (; i < names.length; i++) {
-			defineProperty(object, names[i], descriptors[names[i]]);
+		var names, name;
+		names = keys(descriptors);
+		while ((name = names.pop())) {
+			defineProperty(object, name, descriptors[name]);
 		}
 		return object;
 	}
 
 	function isExtensible (object) {
-		return true;
+		var prop = '_poly_';
+		// create unique property name
+		while (prop in object) prop += '_';
+		// try to set it
+		try {
+			object[prop] = 1;
+			return hasProp(object, prop);
+		}
+		finally {
+			delete object[prop];
+		}
 	}
 
 	function preventExtensions (object) {
@@ -144,12 +190,14 @@ define(['./_base', 'exports'], function (base, exports) {
 	}
 
 	function getOwnPropertyDescriptor (object, name) {
-		return object[name] && {
-			value: object[name],
-			enumerable: true,
-			configurable: true,
-			writable: true
-		};
+		return hasProp(object, name)
+			? {
+				value: object[name],
+				enumerable: true,
+				configurable: true,
+				writable: true
+			}
+			: undef;
 	}
 
 	function createShims (failTest) {
