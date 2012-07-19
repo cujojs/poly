d5b74ec868fe39469d47ba1b1a0bdf749107fff4
John Hann <john@unscriptable.com>
2012-07-19 12:57:24 -0400

    use and expose new setWhitespaceChars

--- all.js
+++ all.js
@@ -1,7 +1,13 @@
-define(['./object', './array', './function', './json', './string', './xhr'], function (object) {
+define(['./object', './string', './array', './function', './json', './xhr'], function (object, string) {
+
+	// set unshimmable Object methods to be forgiving:
+	object.failIfShimmed(false);
+	// set simple whitepsace chars
+	string.setWhitespaceChars('\\s');
 
 	return {
-		failIfShimmed: object.failIfShimmed
+		failIfShimmed: object.failIfShimmed,
+		setWhitespaceChars: string.setWhitespaceChars
 	};
 
 });
