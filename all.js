5e2814aa612e9541156cd901a962d89e2956ecc0
John Hann <john@unscriptable.com>
2012-07-19 13:04:37 -0400

    make poly/all just use default "strictness"

--- all.js
+++ all.js
@@ -1,10 +1,5 @@
 define(['./object', './string', './array', './function', './json', './xhr'], function (object, string) {
 
-	// set unshimmable Object methods to be forgiving:
-	object.failIfShimmed(false);
-	// set simple whitepsace chars
-	string.setWhitespaceChars('\\s');
-
 	return {
 		failIfShimmed: object.failIfShimmed,
 		setWhitespaceChars: string.setWhitespaceChars
