bce9c7135428750a96b7eb7020b037183b9d4bcd
John Hann <john@unscriptable.com>
2012-07-20 21:31:45 -0400

    remove leftover dependency on curl.js

--- json.js
+++ json.js
@@ -8,7 +8,7 @@
  * Licensed under the MIT License at:
  * 		http://www.opensource.org/licenses/mit-license.php
  *
- * TODO: document that JSON module is always downloaded st run-time unless
+ * TODO: document that JSON module is always downloaded at run-time unless
  * dev explicitly mentions it in build instructions
  */
 define(['./lib/_async!./lib/_json'], function (JSON) {
