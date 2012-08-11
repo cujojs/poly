94d1c9e2bf69875cdf0b2ce4612a3d37e9a62140
John Hann <john@unscriptable.com>
2012-08-10 22:29:35 -0400

    stub event shim module

---
+++ event.js
@@ -0,0 +1,14 @@
+/**
+ * DOM event polyfill / shim
+ *
+ * (c) copyright 2011-2012 Brian Cavalier and John Hann
+ *
+ * poly is part of the cujo.js family of libraries (http://cujojs.com/)
+ *
+ * Licensed under the MIT License at:
+ * 		http://www.opensource.org/licenses/mit-license.php
+ *
+ */
+define(['./lib/_base'], function (base) {
+	return {};
+});
