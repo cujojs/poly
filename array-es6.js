bb18bde421665cfad2f1e8b0e8f8dabd1d6ed113
unscriptable <john@unscriptable.com>
2013-07-25 11:04:57 -0400

    Add Array.from and Array.of

--- array-es6.js
+++ array-es6.js
@@ -1,24 +1,45 @@
 // https://gist.github.com/rwldrn/5079436
 // https://gist.github.com/rwldrn/5079427
+// https://gist.github.com/rwldrn/1074126
 define(['./lib/_base', './lib/_array'], function (base, array) {
 "use strict";
 
-	var proto = Array.prototype,
-		featureMap,
+	var ctor = Array,
+		proto = ctor.prototype,
+		slice = proto.slice,
+		protoFeatureMap,
+		ctorFeatureMap,
 		_findIndex;
 
-	featureMap = {
+	protoFeatureMap = {
 		'array-find': 'find',
 		'array-findIndex': 'findIndex'
 	};
 
+	ctorFeatureMap = {
+		'array-from': 'from',
+		'array-of': 'of'
+	};
+
 	function has (feature) {
-		var prop = featureMap[feature];
-		return base.isFunction(proto[prop]);
+		var prop = protoFeatureMap[feature];
+		if (prop) {
+			return base.isFunction(proto[prop]);
+		}
+		prop = ctorFeatureMap[feature];
+		return base.isFunction(ctor[prop]);
+	}
+
+	if (!has('array-from')) {
+		Array.from = function (o) { return slice.call(o); }
+	}
+
+	if (!has('array-of')) {
+		Array.of = function () { return slice.call(arguments); }
 	}
 
 	if (!has('array-findIndex') || !has('array-find')) {
-		_findIndex = function _findIndex (lambda /*, thisArg */) {
+		_findIndex = function findIndexImpl (lambda /*, thisArg */) {
 
 			var foundAt = -1;
 
@@ -30,7 +51,7 @@ define(['./lib/_base', './lib/_array'], function (base, array) {
 			// arguments[+1] is to fool google closure compiler into NOT adding a function argument!
 			}, array.returnValue, arguments[+1]);
 			return foundAt;
-		}
+		};
 
 		if (!has('array-findIndex')) {
 			proto.findIndex = function findIndex (lambda) {
