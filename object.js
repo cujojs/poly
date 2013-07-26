c5a7db65a3ec4266e443cb89ffffe534bdb19fa7
unscriptable <john@unscriptable.com>
2013-07-26 17:08:02 -0400

    Serious code cleanup and prevent IE8 from falsely failing some tests.

--- object.js
+++ object.js
@@ -49,7 +49,7 @@
  * TODO: fix Object#propertyIsEnumerable for IE's non-enumerable props to match Object.keys()
  */
 define(['./lib/_base'], function (base) {
-"use strict";
+'use strict';
 
 	var refObj,
 		refProto,
@@ -62,8 +62,8 @@ define(['./lib/_base'], function (base) {
 		secrets,
 		protoSecretProp,
 		hasOwnProp = 'hasOwnProperty',
-		objectDefineProperty,
-		objectGetOwnPropertyDescriptor,
+		doc = typeof document != 'undefined' && document,
+		testEl = doc && doc.createElement('p'),
 		undef;
 
 	refObj = Object;
@@ -114,55 +114,25 @@ define(['./lib/_base'], function (base) {
 		'object-getprototypeof': 'getPrototypeOf',
 		'object-keys': 'keys',
 		'object-getownpropertynames': 'getOwnPropertyNames',
-		'object-defineproperty': 'defineProperty',
-		'object-defineproperty-domexclusive': function hasDefinePropertyForDom (object) {
-			// return true if the method works ONLY on DOM, but fail on any other object.
-			if (!('defineProperty' in Object)) {
-				return false;
-			}
-
-			var tester = document.createElement('div');
-
-			if (!("sentinel" in Object.defineProperty(tester, "sentinel", { value: 1 }))){
-				return false;
-			}
-
-			try {
-				// should failed on normal object
-				if ("sentinel" in Object.defineProperty({}, "sentinel", { value: 1 })){
-					return false;
-				}
-			}
-			catch (e) {
-			}
-
-			return true;
-		},
-		'object-defineproperties': 'defineProperties',
 		'object-isextensible': 'isExtensible',
 		'object-preventextensions': 'preventExtensions',
-		'object-getownpropertydescriptor': 'getOwnPropertyDescriptor',
-		'object-getownpropertydescriptor-domexclusive': function hasGetOwnPropertyDescriptorObject(object) {
-			if (!('getOwnPropertyDescriptor' in Object)) {
-				return false;
-			}
-
-			var tester = document.createElement('div');
-			tester.sentinel = true;
-
-			if (!(Object.getOwnPropertyDescriptor(tester, "sentinel").value)){
-				return false;
-			}
-
-			try {
-				if (Object.getOwnPropertyDescriptor({ "sentinel" : true }, "sentinel").value){
-					return false;
-				}
-			}
-			catch (e) {
-			}
-
-			return true;
+		'object-defineproperty-obj': function () {
+			return hasDefineProperty({});
+		},
+		'object-defineproperty-dom': function () {
+			return doc && hasDefineProperty(testEl);
+		},
+		'object-defineproperties-obj': function () {
+			return hasDefineProperties({});
+		},
+		'object-defineproperties-dom': function () {
+			return doc && hasDefineProperties(testEl);
+		},
+		'object-getownpropertydescriptor-obj': function () {
+			return hasGetOwnPropertyDescriptor({});
+		},
+		'object-getownpropertydescriptor-dom': function () {
+			return doc && hasGetOwnPropertyDescriptor(testEl);
 		}
 	};
 
@@ -174,29 +144,112 @@ define(['./lib/_base'], function (base) {
 
 	protoSecretProp = !has('object-getprototypeof') && !has__proto__ && hasNonEnumerableProps && hasOwnProp;
 
-	function createFlameThrower (feature) {
-		return function () {
-			throw new Error('poly/object: ' + feature + ' is not safely supported.');
+	// we might create an owned property to hold the secrets, but make it look
+	// like it's not an owned property.  (affects getOwnPropertyNames, too)
+	if (protoSecretProp) (function (_hop) {
+		refProto[hasOwnProp] = function (name) {
+			if (name == protoSecretProp) return false;
+			return _hop.call(this, name);
+		};
+	}(refProto[hasOwnProp]));
+
+	if (!has('object-create')) {
+		Object.create = shims.create = create;
+	}
+
+	if (!has('object-freeze')) {
+		Object.freeze = shims.freeze = noop;
+	}
+
+	if (!has('object-isfrozen')) {
+		Object.isFrozen = shims.isFrozen = nope;
+	}
+
+	if (!has('object-seal')) {
+		Object.seal = shims.seal = noop;
+	}
+
+	if (!has('object-issealed')) {
+		Object.isSealed = shims.isSealed = nope;
+	}
+
+	if (!has('object-getprototypeof')) {
+		Object.getPrototypeOf = shims.getPrototypeOf = getPrototypeOf;
+	}
+
+	if (!has('object-keys')) {
+		Object.keys = keys;
+	}
+
+	if (!has('object-getownpropertynames')) {
+		Object.getOwnPropertyNames = shims.getOwnPropertyNames
+			= getOwnPropertyNames;
+	}
+
+	if (!has('object-defineproperties-obj')) {
+		// check if dom has it (IE8)
+		Object.defineProperties = shims.defineProperties
+			= has('object-defineproperties-dom')
+				? useNativeForDom(Object.defineProperties, defineProperties)
+				: defineProperties;
+	}
+
+	if (!has('object-defineproperty-obj')) {
+		// check if dom has it (IE8)
+		Object.defineProperty = shims.defineProperty
+			= has('object-defineproperty-dom')
+				? useNativeForDom(Object.defineProperty, defineProperty)
+				: defineProperty;
+	}
+
+	if (!has('object-isextensible')) {
+		Object.isExtensible = shims.isExtensible = isExtensible;
+	}
+
+	if (!has('object-preventextensions')) {
+		Object.preventExtensions = shims.preventExtensions = preventExtensions;
+	}
+
+	if (!has('object-getownpropertydescriptor-obj')) {
+		// check if dom has it (IE8)
+		Object.getOwnPropertyDescriptor = shims.getOwnPropertyDescriptor
+			= has('object-getownpropertydescriptor-dom')
+				? useNativeForDom(Object.getOwnPropertyDescriptor, getOwnPropertyDescriptor)
+				: getOwnPropertyDescriptor;
+	}
+
+	function hasDefineProperty (object) {
+		if (('defineProperty' in Object)) {
+			try {
+				// test it
+				Object.defineProperty(object, 'sentinel1', { value: 1 })
+				return 'sentinel1' in object;
+			}
+			catch (ex) { /* squelch */ }
 		}
 	}
 
-	function has (feature) {
-		var ret;
-		var prop = featureMap[feature];
+	// Note: MSDN docs say that IE8 has this function, but tests show
+	// that it does not! JMH
+	function hasDefineProperties (object) {
+		if (('defineProperties' in Object)) {
+			try {
+				// test it
+				Object.defineProperties(object, { 'sentinel2': { value: 1 } })
+				return 'sentinel2' in object;
+			}
+			catch (ex) { /* squelch */ }
+		}
+	}
 
-		if (base.isFunction(prop)) {
-			// cache return value
-			ret = featureMap[feature] = prop(refObj);
+	function hasGetOwnPropertyDescriptor (object) {
+		if (('getOwnPropertyDescriptor' in Object)) {
+			object['sentinel3'] = true;
+			try {
+				return (Object.getOwnPropertyDescriptor(object, 'sentinel3').value);
+			}
+			catch (ex) { /* squelch */ }
 		}
-		else if (base.isString(prop)) {
-			ret = featureMap[feature] = prop in refObj;
-		}
-		else {
-			// return cached evaluate result
-			ret = prop;
-		}
-			
-		return ret;
 	}
 
 	function PolyBase () {}
@@ -216,12 +269,48 @@ define(['./lib/_base'], function (base) {
 		return result;
 	}
 
-	function shimDefineProperty (object, name, descriptor) {
+	function create (proto, props) {
+		var obj;
+
+		if (typeof proto != 'object') throw new TypeError('prototype is not of type Object or Null.');
+
+		PolyBase.prototype = proto;
+		obj = new PolyBase();
+		PolyBase.prototype = null;
+
+		// provide a mechanism for retrieving the prototype in IE 6-8
+		if (protoSecretProp) {
+			var orig = obj[protoSecretProp];
+			obj[protoSecretProp] = function (name) {
+				if (name == secrets) return true; // yes, we're using secrets
+				if (name == secrets.proto) return proto;
+				return orig.call(this, name);
+			};
+		}
+
+		if (arguments.length > 1) {
+			// defineProperties could throw depending on `failIfShimmed`
+			Object.defineProperties(obj, props);
+		}
+
+		return obj;
+	}
+
+	function defineProperties (object, descriptors) {
+		var names, name;
+		names = keys(descriptors);
+		while ((name = names.pop())) {
+			Object.defineProperty(object, name, descriptors[name]);
+		}
+		return object;
+	}
+
+	function defineProperty (object, name, descriptor) {
 		object[name] = descriptor && descriptor.value;
 		return object;
 	}
 
-	function shimGetOwnPropertyDescriptor (object, name) {
+	function getOwnPropertyDescriptor (object, name) {
 		return hasProp(object, name)
 			? {
 				value: object[name],
@@ -232,143 +321,33 @@ define(['./lib/_base'], function (base) {
 			: undef;
 	}
 
-	// we might create an owned property to hold the secrets, but make it look
-	// like it's not an owned property.  (affects getOwnPropertyNames, too)
-	if (protoSecretProp) (function (_hop) {
-		refProto[hasOwnProp] = function (name) {
-			if (name == protoSecretProp) return false;
-			return _hop.call(this, name);
-		};
-	}(refProto[hasOwnProp]));
-
-	if (!has('object-create')) {
-		Object.create = shims.create = function create (proto, props) {
-			var obj;
-
-			if (typeof proto != 'object') throw new TypeError('prototype is not of type Object or Null.');
-
-			PolyBase.prototype = proto;
-			obj = new PolyBase();
-			PolyBase.prototype = null;
-
-			// provide a mechanism for retrieving the prototype in IE 6-8
-			if (protoSecretProp) {
-				var orig = obj[protoSecretProp];
-				obj[protoSecretProp] = function (name) {
-					if (name == secrets) return true; // yes, we're using secrets
-					if (name == secrets.proto) return proto;
-					return orig.call(this, name);
-				};
-			}
-
-			if (arguments.length > 1) {
-				// defineProperties could throw depending on `failIfShimmed`
-				Object.defineProperties(obj, props);
-			}
-
-			return obj;
-		};
+	function getOwnPropertyNames (object) {
+		return keys(object);
 	}
 
-	if (!has('object-freeze')) {
-		Object.freeze = shims.freeze = function freeze (object) {
-			return object;
-		};
+	function isExtensible (object) {
+		var prop = '_poly_';
+		try {
+			// create unique property name
+			while (prop in object) prop += '_';
+			// try to set it
+			object[prop] = 1;
+			return hasProp(object, prop);
+		}
+		catch (ex) { return false; }
+		finally {
+			try { delete object[prop]; } catch (ex) { /* squelch */ }
+		}
 	}
 
-	if (!has('object-isfrozen')) {
-		Object.isFrozen = shims.isFrozen = function isFrozen (object) {
-			return false;
-		};
+	function preventExtensions (object) {
+		return object;
 	}
 
-	if (!has('object-seal')) {
-		Object.seal = shims.seal = function seal (object) {
-			return object;
-		};
-	}
-
-	if (!has('object-issealed')) {
-		Object.isSealed = shims.isSealed = function isSealed (object) {
-			return false;
-		};
-	}
-
-	if (!has('object-getprototypeof')) {
-		Object.getPrototypeOf = shims.getPrototypeOf = getPrototypeOf;
-	}
-
-	if (!has('object-keys')) {
-		Object.keys = keys;
-	}
-
-	if (!has('object-getownpropertynames')) {
-		Object.getOwnPropertyNames = shims.getOwnPropertyNames = function getOwnPropertyNames (object) {
-			return keys(object);
-		};
-	}
-
-	if (!has('object-defineproperty')) {
-		Object.defineProperty = shims.defineProperty = shimDefineProperty;
-	}
-	else if(has('object-defineproperty-domexclusive')) {
-		objectDefineProperty = Object.defineProperty;
-		Object.defineProperty = shims.defineProperty = function defineProperty (object, name, descriptor) {
-			if (base.isElement(object)) {
-				return objectDefineProperty.apply(Object, arguments);
-			}
-			else {
-				return shimDefineProperty.apply(Object, arguments);
-			}
-		};
-	}
-
-	if (!has('object-defineproperties') || !has('object-create')) {
-		Object.defineProperties = shims.defineProperties = function defineProperties (object, descriptors) {
-			var names, name;
-			names = keys(descriptors);
-			while ((name = names.pop())) {
-				Object.defineProperty(object, name, descriptors[name]);
-			}
-			return object;
-		};
-	}
-
-	if (!has('object-isextensible')) {
-		Object.isExtensible = shims.isExtensible = function isExtensible (object) {
-			var prop = '_poly_';
-			try {
-				// create unique property name
-				while (prop in object) prop += '_';
-				// try to set it
-				object[prop] = 1;
-				return hasProp(object, prop);
-			}
-			catch (ex) { return false; }
-			finally {
-				try { delete object[prop]; } catch (ex) { /* squelch */ }
-			}
-		};
-	}
-
-	if (!has('object-preventextensions')) {
-		Object.preventExtensions = shims.preventExtensions = function preventExtensions (object) {
-			return object;
-		};
-	}
-
-	if (!has('object-getownpropertydescriptor')) {
-		Object.getOwnPropertyDescriptor = shims.getOwnPropertyDescriptor = shimGetOwnPropertyDescriptor;
-	}
-	else if (has('object-getownpropertydescriptor-domexclusive')) {
-		objectGetOwnPropertyDescriptor = Object.getOwnPropertyDescriptor;
-		Object.getOwnPropertyDescriptor = shims.getOwnPropertyDescriptor = function getOwnPropertyDescriptor (object, name) {
-			if (base.isElement(object)) {
-				return objectGetOwnPropertyDescriptor.apply(Object, arguments);
-			}
-			else {
-				return shimGetOwnPropertyDescriptor.apply(Object, arguments)
-			}
+	function useNativeForDom (orig, shim) {
+		return function (obj) {
+			if (base.isElement(obj)) return orig.apply(this, arguments);
+			else return shim.apply(this, arguments);
 		};
 	}
 
@@ -391,7 +370,40 @@ define(['./lib/_base'], function (base) {
 		}
 	}
 
-	function assertIsObject (o) { if (typeof o != 'object') throw new TypeError('Object.getPrototypeOf called on non-object'); }
+	function assertIsObject (o) {
+		if (typeof o != 'object') {
+			throw new TypeError('Object.getPrototypeOf called on non-object');
+		}
+	}
+
+	function createFlameThrower (feature) {
+		return function () {
+			throw new Error('poly/object: ' + feature + ' is not safely supported.');
+		}
+	}
+
+	function has (feature) {
+		var ret;
+		var prop = featureMap[feature];
+
+		if (base.isFunction(prop)) {
+			// cache return value, ensure boolean
+			ret = featureMap[feature] = !!prop(refObj);
+		}
+		else if (base.isString(prop)) {
+			ret = featureMap[feature] = prop in refObj;
+		}
+		else {
+			// return cached evaluate result
+			ret = prop;
+		}
+
+		return ret;
+	}
+
+	function noop (it) { return it; }
+
+	function nope (it) { return false; }
 
 	return {
 		failIfShimmed: failIfShimmed
